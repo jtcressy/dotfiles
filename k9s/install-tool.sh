58e25c7ff0dcf59672f205a0ab386a326803b123
Joel Cressy <joel@jtcressy.net>
2022-09-01 12:23:57 -0500

    Update install-tool.sh

--- k9s/install-tool.sh
+++ k9s/install-tool.sh
@@ -2,5 +2,5 @@
 
 mkdir -p $HOME/.k9s
 
-ln -sfn k9s/config.yaml $HOME/.k9s/
-ln -sfn k9s/skin.yaml $HOME/.k9s/
+ln -sfn ${DOTFILES_ROOT}/k9s/config.yaml $HOME/.k9s/
+ln -sfn ${DOTFILES_ROOT}/k9s/skin.yaml $HOME/.k9s/

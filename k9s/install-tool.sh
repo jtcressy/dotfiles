42c7e9d7244e48005c3381e2aaff074ea265c1ea
Joel Cressy <joel@jtcressy.net>
2023-01-04 14:36:02 -0600

    Update install-tool.sh

--- k9s/install-tool.sh
+++ k9s/install-tool.sh
@@ -6,11 +6,11 @@ SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-SCRIPT_NAME=${1:-"install.sh"}
+SCRIPT_NAME=${1:-"install-tool.sh"}
 
 #TODO: install k9s agnostically
 
 mkdir -p $HOME/.k9s
 
-ln -sfn ${SCRIPTPATH}/k9s/config.yaml $HOME/.k9s/
-ln -sfn ${SCRIPTPATH}/k9s/skin.yaml $HOME/.k9s/
+ln -sfn ${SCRIPTPATH}/config.yaml $HOME/.k9s/
+ln -sfn ${SCRIPTPATH}/skin.yaml $HOME/.k9s/
